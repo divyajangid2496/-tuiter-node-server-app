a8e0d0c55764d1023acf00c0c314c3d0cfd376e6
Divya Daulat Jangid <divyajangid2496@gmail.com>
2023-04-10 00:17:03 -0500

    a9 Mongo fa22

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb+srv://giuseppi:supersecretpassword@divyacluster.tdb0ywh.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(CONNECTION_STRING);
 
 import HelloController from "./controllers/hello-controller.js"
 import UserController from "./controllers/users/users-controller.js"
@@ -7,7 +10,9 @@ import TuitsController from "./controllers/tuits/tuits-controller.js";
 
 
 const app = express();
+
 app.use(cors());
+
 app.use(express.json());
 
 HelloController(app);
--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -1,6 +1,5 @@
-import posts from "./tuits.js";
-
-let tuits = posts;
+// in a9, retrieve data from mongodb
+import * as tuitsDao from '../tuits/tuits-dao.js'
 
 const currentUser = {
     "userName": "NASA",
@@ -20,35 +19,69 @@ const templateTuit = {
     "dislikes": 0,
 }
 
-const createTuit = (req, res) => {
+const findTuits = async (req, res) => {
+    const tuits = await tuitsDao.findTuits();
+    res.json(tuits);
+}
+
+const createTuit = async (req, res) => {
     let newTuit = req.body;
-    newTuit._id = (new Date()).getTime();
     newTuit = {
         ...templateTuit,
         ...newTuit
-    }
-    tuits.unshift(newTuit);
-    res.json(newTuit);
+    };
+    const insertedTuit = await tuitsDao.createTuit(newTuit);
+    res.json(insertedTuit);
 }
 
-const findTuits  = (req, res) => res.json(tuits);
+const deleteTuit = async (req, res) => {
+    const tuitIdToDelete = req.params.tid;
+    const status = await tuitsDao.deleteTuit(tuitIdToDelete);
+    // have to return json, since the returned status is a json object
+    res.json(status);
+}
 
-const updateTuit = (req, res) => {
+const updateTuit = async (req, res) => {
     const tuitIdToUpdate = req.params.tid;
     const updates = req.body;
-    const tuitsIndex = tuits.findIndex(tuit => tuit._id === parseInt(tuitIdToUpdate));
-    tuits[tuitsIndex] = {
-        ...tuits[tuitsIndex],
-        ...updates
-    };
-    res.sendStatus(200);
+    const status = await tuitsDao.updateTuit(tuitIdToUpdate, updates);
+    res.json(status);
 }
 
-const deleteTuit = (req, res) => {
-    const tuitIdToDelete = req.params['tid'];
-    tuits = tuits.filter(tuit => tuit._id !== parseInt(tuitIdToDelete));
-    res.sendStatus(200);
-}
+// in a8, we retrieve tuits data from self-defined js file
+// import posts from "./tuits.js";
+//
+// let tuits = posts;
+//
+// const createTuit = (req, res) => {
+//     let newTuit = req.body;
+//     newTuit._id = (new Date()).getTime();
+//     newTuit = {
+//         ...templateTuit,
+//         ...newTuit
+//     }
+//     tuits.unshift(newTuit);
+//     res.json(newTuit);
+// }
+//
+// const findTuits  = (req, res) => res.json(tuits);
+//
+// const updateTuit = (req, res) => {
+//     const tuitIdToUpdate = req.params.tid;
+//     const updates = req.body;
+//     const tuitsIndex = tuits.findIndex(tuit => tuit._id === parseInt(tuitIdToUpdate));
+//     tuits[tuitsIndex] = {
+//         ...tuits[tuitsIndex],
+//         ...updates
+//     };
+//     res.sendStatus(200);
+// }
+//
+// const deleteTuit = (req, res) => {
+//     const tuitIdToDelete = req.params['tid'];
+//     tuits = tuits.filter(tuit => tuit._id !== parseInt(tuitIdToDelete));
+//     res.sendStatus(200);
+// }
 
 export default (app) => {
     app.post('/api/tuits', createTuit);
